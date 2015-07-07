3e9b54492ea9f631cdc36637a3286167bb3d1290
tlodge <tlodge@gmail.com>
2015-07-07 11:58:05 +0000

    cp commit

--- server/db.js
+++ server/db.js
@@ -23,32 +23,56 @@ module.exports = {
       });
     },
 
-    fetch_latest_ts_for_hosts: function(hosts){
-
+    /*fetch_latest_ts_for_hosts: function(hosts){
+      console.log("laetst ts for hosts!");
       var results = [];
 
       return db.serializeAsync().then(function(){
 
         var hstr = hosts.map(function(host){return "\'" + host + "\'"}).join();
 
-        //how do we paramaterise this?
+        //how do we parameterise this?
 
         var sql = "SELECT max(u.ts) as ts FROM URLS u WHERE u.host IN (" + hstr + ") AND ts != ''";
 
-        return db.allAsync(sql, function(err,rows){
-
-            if (err){
-              return []
-            }
-            else{
-              return rows;
-            }
+        return db.allAsync(sql).then(function(rows){
+            return rows[0];
+        }, function(err){
+            console.log(err);
+            return [];
         });
       });
+    },*/
+
+    fetch_latest_ts_for_hosts: function(hosts){
+
+      var hstr = hosts.map(function(host){return "\'" + host + "\'"}).join();
+      var sql = "SELECT max(u.ts) as ts FROM URLS u WHERE u.host IN (" + hstr + ") AND ts != ''";
+
+      return db.serializeAsync()
+
+        .then(function(){
+          return db.allAsync(sql)
+        })
+
+        .then(function(rows){
+          return rows[0];
+        },function(err){
+            console.log(err);
+            return [];
+        });
     },
 
-    fetch_browsing_for_hosts: function(hosts, fromts, tots){
+    fetch_browsing_for_hosts: function(hosts, from, to){
 
+        var hstr = hosts.map(function(host){return "\'" + host + "\'"}).join();
+        var sql = "SELECT DISTINCT u.ts, u.tld, u.host from URLS u WHERE u.host IN ("+hstr+") AND (u.ts >= "+from+" AND u.ts <= "+to+") ORDER BY u.host, u.ts ASC";
+
+        return db.serializeAsync().then(function(){
+            return db.allAsync(sql);
+        }).then(function (rows){
+            return rows;
+        });
     },
 
 
--- server/server.js
+++ server/server.js
@@ -3,18 +3,18 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var hbs = require('hbs');
 var db = require('./db');
+var util = require('./util');
 var app = express();
 
 //to support POSTs
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
-app.use('/', express.static("static"));
+app.use('/hv/', express.static("static"));
 app.set('view engine', 'html');
 
 app.engine('html', hbs.__express);
 var server = http.createServer(app);
-
-//db.fetch_hosts();
+var AWEEKAGO = 7 * 24 * 60 * 60;
 
 var hosts = ["10.2.0.6", "10.2.0.5", "10.1.0.6", "10.1.0.5"];
 
@@ -27,21 +27,19 @@ app.get('/hv/test', function(req, res){
   });
 }),
 
-/*app.get('/hv/browsing', function(req,res){
-
-  console.log("ok seen hv browsing!");
-
-  db.fetch_latest_ts_for_hosts(hosts).then(function(timerange){
-    return timerange;
+app.get('/hv/browsing', function(req,res){
+  db.fetch_latest_ts_for_hosts(hosts).then(function(to){
+    return {from:to.ts - AWEEKAGO, to:to.ts}
   }).then(function(timerange){
-    return fetch_browsing_for_hosts(hosts, timerange.from, timerange.to)
+    return db.fetch_browsing_for_hosts(hosts, timerange.from, timerange.to)
   }).then(function(raw){
-    var binned = util.binned(bin, raw['results'])
+    var bin = 60 * 60; //hourly bin!
+    var binned = util.binned(bin, raw);
     res.send({
       raw    : raw,
       binned : binned,
     });
   });
-});*/
+});
 
 server.listen(8080);
